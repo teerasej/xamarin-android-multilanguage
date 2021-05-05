3ce3c53d3f4f78ba8e2b3220afc0cbab5a86a0e0
Teerasej <teerasej@gmail.com>
2021-05-05 21:57:00 +0700

    โหลด context ใหม่โดยกำหนดรหัสภาษาที่เตรียมไว้

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using Java.Util;
 
 namespace multilanguage
 {
@@ -14,6 +17,23 @@ namespace multilanguage
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var buttonSwitchLanguage = FindViewById<Button>(Resource.Id.buttonSwitchLanguage);
+            var textHello = FindViewById<TextView>(Resource.Id.textHello);
+
+            buttonSwitchLanguage.Click += (sender, e) =>
+            {
+                var languageCode = "th";
+
+                Configuration overrideConfiguration = this.Resources.Configuration;
+                Locale locale = new Locale(languageCode);
+                overrideConfiguration.SetLocale(locale);
+
+                var context = CreateConfigurationContext(overrideConfiguration);
+                var stringHello = context.GetString(Resource.String.hello);
+                textHello.Text = stringHello;
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
